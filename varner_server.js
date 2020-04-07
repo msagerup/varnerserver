0f7917f53727a8f496c374a1946c2158093720f5
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-04-07 15:35:31 +0200

    Update varner_server.js

--- varner_server.js
+++ varner_server.js
@@ -4,7 +4,6 @@ const app = express()
 const port = process.env.PORT || 8000;
 
 app.get('/data', function(req, res){
-    // console.log('hello')
     res.contentType('application/xml');
     res.sendFile(path.join(__dirname , 'order1.xml'));
 });
