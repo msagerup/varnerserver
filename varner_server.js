11390681af05eef8ba374439486704cd9be70669
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-04-07 15:35:11 +0200

    Update varner_server.js

--- varner_server.js
+++ varner_server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-var path = require('path');
+const path = require('path');
 const app = express()
 const port = process.env.PORT || 8000;
 
@@ -9,7 +9,7 @@ app.get('/data', function(req, res){
     res.sendFile(path.join(__dirname , 'order1.xml'));
 });
 
-var server = app.listen(port, () => {
-	console.log('Started listening on 8080');
+const server = app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
 });
 
